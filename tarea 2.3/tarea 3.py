71f1c012399a8910dfa61dd58e5ecea298428493
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-09-24 20:23:24 -0600

    Add files via upload

---
+++ tarea 2.3/tarea 3.py
@@ -0,0 +1,20 @@
+#esta linea indicara mi nombre
+
+#Arzaba Diaz April 
+
+#esta linea dira el numero de programa que are
+
+#tarea 2.3
+
+
+#esta linea solicitara la temperatura en Fahrenheit
+
+fahrenheit = float(input("Ingresa la temperatura en grados Fahrenheit:"))
+
+#esta linea lo convertira a Celsius
+
+celsius = (5 / 9) * (fahrenheit - 32)
+
+#esta linea mostrara el resultado
+
+print(f"La temperatura en grados Celsius es: {celsius:.2f}")
